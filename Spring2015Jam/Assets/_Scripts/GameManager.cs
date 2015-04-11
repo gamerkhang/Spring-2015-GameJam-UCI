3db4c2f5d8532b531166ba54d97c027d5fc621bf
Khang Tran <gamerkhang@gmail.com>
2015-04-11 10:36:13 -0700

    asdf

--- Spring2015Jam/Assets/_Scripts/GameManager.cs
+++ Spring2015Jam/Assets/_Scripts/GameManager.cs
@@ -4,43 +4,72 @@ using System.Collections;
 public class GameManager : MonoBehaviour {
 	//Clock array
 	public GameObject[] spawnableClocks;
-	public int gameHour = 12, gameMinute = 0;
-	int lives = 5;
-	bool gameRunning = true;
+	public static int gameHour = 12, gameMinute = 0;
+	public int lives = 5;
+	public int minClocks, maxClocks;
+	public GameObject pauseMenu, gameOverMenu;
 
 	// Use this for initialization
 	void Start () {
-
+		InvokeRepeating("UpdateUniversalClock", 1.0f, 5.0f);
+		pauseMenu = GameObject.Find("PauseMenu");
+		gameOverMenu = GameObject.Find("GameOverMenu");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (gameRunning) {
+		if (Time.timeScale == 0)
+			return;
 
-		}
+
 	}
 
 	void FixedUpdate () {
-		if (gameRunning) {
-			//Universal clock to sync w/
+		//Running this depends on Timescale
+		//Universal clock to sync w/
+		Debug.Log(gameHour + ":" + gameMinute);
+	}
 
+	void UpdateUniversalClock () {
+		gameMinute++;
 
+		if (gameMinute >= 60)
+		{
+			gameMinute = 0;
+			gameHour++;
+
+			if (gameHour >= 13)
+				gameHour = 1;
 		}
 	}
 
-	void AddClock () {
+	void SpawnClock () {
 		//Instantiate(spawnableClocks[Random.Range (0, spawnableClocks.Length)]);  position?
 	}
+	
+	public void AddLife () {
+		lives++;
+	}
 
-	public void LoseLife() {
+	public void LoseLife () {
 		lives--;
 		if (lives <= 0) {
-			Time.timeScale = 0;
+			GameOver();
 			//make button to try again or quit active; try again button reloads the scene.
 		}
 	}
 
-	public void AddLife () {
-		lives++;
+	void GameOver () {
+		Time.timeScale = 0;
+		//gameOverMenu.SetActive(true);
+	}
+	public void Pause () {
+		Time.timeScale = 0;
+		//pauseMenu.SetActive(true);
+	}
+
+	public void Unpause () {
+		//pauseMenu.SetActive(false);
+		Time.timeScale = 1;
 	}
 }
