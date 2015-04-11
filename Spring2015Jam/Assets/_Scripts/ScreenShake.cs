b80d643df7e44df25f188a48e95a78fc01c360fc
JerickoOrdonez <Jericko.Ordonez@gmail.com>
2015-04-11 16:15:08 -0700

    clock scene

---
+++ Spring2015Jam/Assets/_Scripts/ScreenShake.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenShake : MonoBehaviour 
+{
+	public static CameraShake Instance;
+
+	private float _amplitude = 0.1;
+	private float _duration = 0.5f;
+
+	private Vector3 initialPosition;
+	private bool isShaking = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Instance = this;
+		initialPosition = transform.localPosition;
+	}
+
+	public void Shake(float amplitude, float duration)
+	{
+		isShaking = true
+		CancelInvoke();
+		Invoke("StopShaking", duration);
+	}
+
+	public void StopShaking()
+	{
+		isShaking = false
+	}
+	// Update is called once per frame
+	void Update () 
+	{
+		if(isShaking)
+		{
+			transform.localPosition = initialPosition + Random.insideUnitSphere+amplitude;
+		}
+	}
+}
