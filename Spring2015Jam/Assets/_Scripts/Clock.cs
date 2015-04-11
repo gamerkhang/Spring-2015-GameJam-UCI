34b40d05d8ad3b443f2d752206560847b04f02e5
gfazenda <gludvigf@uci.edu>
2015-04-11 10:19:13 -0700

    added clock script

--- Spring2015Jam/Assets/_Scripts/Clock.cs
+++ Spring2015Jam/Assets/_Scripts/Clock.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+
 
 public class Clock : MonoBehaviour {
 	int lifeTime, killRange;
@@ -14,30 +16,33 @@ public class Clock : MonoBehaviour {
 	float delay = 0f;
 	public bool positiveRange = true;
 	float timeChanger = 0f;
-	// Use this for initialization
+	public Text ClockTime;
+
+
 	void Start () {
 		GameManager = GameObject.Find ("GameManager");
 		lifeTime = Random.Range (minLife, maxLife);
 		killRange = Random.Range (minRange, maxRange);
 		currentTime = 770;
-		//min = killRange;
+		InvokeRepeating ("UpdateClockTime", 1.0f, 0.2f);
+		infoCard.gameObject.SetActive (false);
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		delay += 5 * Time.deltaTime;
-		if (delay >= 2) {
-			delay = 0;
-			currentTime += 1;
-//			if (min > 59) {
-//				min = 0;
-//				lifeTime-=1;
-//				hour += 1;
-//				
-//			}
-			CheckLimits();
-			printTime ();
-		}
+//		delay += 5 * Time.deltaTime;
+//		if (delay >= 2) {
+//			delay = 0;
+//
+////			if (min > 59) {
+////				min = 0;
+////				lifeTime-=1;
+////				hour += 1;
+////				
+////			}
+//			CheckLimits();
+//			printTime ();
+//		}
 
 
 	}
@@ -48,21 +53,28 @@ public class Clock : MonoBehaviour {
 		//	Debug.Log ("explosion");
 	}
 
-	void printTime(){
+	void UpdateClockTime(){
+		currentTime += 1;
+		lifeTime -= 1;
 		hour = currentTime / 60;
 		if (hour > 12) {
-			hour = hour-12;
+			hour = hour-(12*(hour/12));
 		}
 		min = currentTime % 60;
+		CheckLimits ();
+		printTime ();
+	}
+
+	void printTime(){
 		string minute="";
 		if(min<10)
 			minute = "0";
-		Debug.Log (hour + ":"+minute + min);
+		ClockTime.text = (hour + ":"+minute + min).ToString();
 	}
 
 	void OnMouseOver(){
 		if (Input.GetMouseButton(0)) { //left button
-			timeChanger += .15f;
+			timeChanger += .25f;
 			if( timeChanger > 1){
 				currentTime += (int) timeChanger;
 				timeChanger = 0f;
@@ -70,7 +82,7 @@ public class Clock : MonoBehaviour {
 			//Debug.Log("here");
 		}
 		else if (Input.GetMouseButton(1)) { //right button
-			timeChanger += .15f;
+			timeChanger += .25f;
 			if( timeChanger > 1){
 				currentTime -= (int) timeChanger;
 				timeChanger = 0f;
