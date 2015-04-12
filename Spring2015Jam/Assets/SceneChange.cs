420784f3a70e20daf3cec822b9479f33c2ad9c3d
kgwong <wong.kevin47@gmail.com>
2015-04-12 11:16:09 -0700

    Basic Main Menu

---
+++ Spring2015Jam/Assets/SceneChange.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneChange : MonoBehaviour {
+
+    public void ChangeToScene(string sceneToChangeTo)
+    {
+        Application.LoadLevel(sceneToChangeTo);
+    }
+}
