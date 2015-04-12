e083e98a91a2ad2ca25f3599c1447016f89a31db
Khang Tran <gamerkhang@gmail.com>
2015-04-11 17:50:08 -0700

    moar clockgen

--- Spring2015Jam/Assets/_Scripts/GameManager.cs
+++ Spring2015Jam/Assets/_Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour {
 	//Clock array
@@ -10,16 +11,19 @@ public class GameManager : MonoBehaviour {
 	int gameHour = 0, gameMinute = 0;
 	public static int lives = 5;
 	public static int CurrentUniversalTime = 720;
-	public int currentAmountClocks, maxClocks = 1;
+	public static int currentAmountClocks, maxClocks = 1;
 	public GameObject pauseMenu, gameOverMenu;
 	public static float universalTickRate;
 	public float clockSpawnRate = 30.0f;
+	public float difficultyIncreaseRate = 60.0f;
+	public int maxDifficulty = 8;
 
 	// Use this for initialization
 	void Start () {
 		universalTickRate = 0.2f;
 		InvokeRepeating("UpdateUniversalClock", 0f, universalTickRate);
 		InvokeRepeating("AddClock", 0f, clockSpawnRate);
+		InvokeRepeating("IncreaseMaxClocks", 0f, difficultyIncreaseRate);
 		pauseMenu.SetActive(false);
 		gameOverMenu.SetActive(false);
 	}
@@ -61,19 +65,40 @@ public class GameManager : MonoBehaviour {
 			gameHour -=(12*(gameHour/12));
 		}
 		gameMinute = minutes % 60;
-		string minute="";
-		if(gameMinute<10)
-			minute = "0";
+
 		t.text = string.Format ("{0:D2}:{1:D2}", gameHour, gameMinute);
 	}
 
 
 	void AddClock () {
-		if ()
+		if (currentAmountClocks < maxClocks)
+		{
+			List<GameObject> inactives = new List<GameObject>();
+			foreach (GameObject clock in spawnableClocks)
+			{
+				if (!clock.activeInHierarchy)
+					inactives.Add(clock);
+			}
+			if (inactives.Count > 0)
+			{
+				int clockIndex = Random.Range (0, inactives.Count);
+				Debug.Log (clockIndex);
+				inactives[clockIndex].SetActive(true);	
+				inactives[clockIndex].GetComponent<Clock>().StartClock();
+				currentAmountClocks++;
+			}
+		}
 		//Instantiate(spawnableClocks[Random.Range (0, spawnableClocks.Length)]);  position?
         //set the chosen one active
 	}
-	
+
+	void IncreaseMaxClocks() {
+		if (maxClocks < maxDifficulty)
+			maxClocks *= 2;
+		else
+			CancelInvoke("IncreaseMaxClocks");
+	}
+
 	public void AddLife () {
 		lives++;
 	}
