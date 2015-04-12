d7f30b77489828a3c0ed5f7398a3922f197c5192
Khang Tran <gamerkhang@gmail.com>
2015-04-11 19:00:11 -0700

    moar bullshit

--- Spring2015Jam/Assets/_Scripts/Clock.cs
+++ Spring2015Jam/Assets/_Scripts/Clock.cs
@@ -5,128 +5,128 @@ using UnityEngine.UI;
 
 public class Clock : MonoBehaviour {
 	public int timeToSuccess, failureThreshold;
-
+	
 	public float successTickRate;
 	public float clockTickRate;
-
+	
 	public GameObject infoCard;
-
-    public int minTimeOffset, maxTimeOffset;
+	
+	public int minTimeOffset, maxTimeOffset;
 	public int minSuccessTime, maxSuccessTime;
 	public int minThreshold, maxThreshold;
-
+	
 	public int hour, min, currentTime;
-
+	
 	public bool ClockAlive = true;
-
+	
 	public bool positiveRange = true;
 	
-    public Image HealthBar;
+	public Image HealthBar;
 	public Text TextClock;
 	public Text TextThreshold;
-
-    public float clickChangeRate;
-    float timeChanger = 0f;
- 
+	
+	public float clickChangeRate;
+	float timeChanger = 0f;
+	
 	float LifeMultiplier = 0f;
-
+	
 	void Start () {
 		successTickRate = GameManager.universalTickRate;
-
-        currentTime = GameManager.CurrentUniversalTime + Random.Range(minTimeOffset, maxTimeOffset);
+		
+		currentTime = GameManager.CurrentUniversalTime + Random.Range(minTimeOffset, maxTimeOffset);
 		timeToSuccess = Random.Range (minSuccessTime, maxSuccessTime);
 		failureThreshold = Random.Range (minThreshold, maxThreshold);
-
-        //SliderSuccess.maxValue = timeToSuccess;
-        TextThreshold.text = failureThreshold + " Min";
-
-//		InvokeRepeating ("UpdateClockTime", 0f, clockTickRate);
-//		InvokeRepeating ("UpdateTimeToSuccess", 0f, successTickRate);
-//		infoCard.gameObject.SetActive (false);
-
+		
+		//SliderSuccess.maxValue = timeToSuccess;
+		TextThreshold.text = failureThreshold + " Min";
+		
+		//		InvokeRepeating ("UpdateClockTime", 0f, clockTickRate);
+		//		InvokeRepeating ("UpdateTimeToSuccess", 0f, successTickRate);
+		//		infoCard.gameObject.SetActive (false);
+		
 		LifeMultiplier = (float) 1 / timeToSuccess;
 	}
-
+	
 	void DisplayTime(){
-        hour = currentTime / 60;
+		hour = currentTime / 60;
 		if (hour > 12) {
 			hour -=(12*(hour/12));
 		}
-        min = currentTime % 60;
+		min = currentTime % 60;
 		TextClock.text = string.Format ("{0:D2}:{1:D2}", hour, min);
 	}
-
-
+	
+	
 	void UpdateClockTime(){
 		currentTime += 1;
 		CheckFailure ();
 		DisplayTime ();
 	}
-
+	
 	void CheckFailure(){
-        if (Mathf.Abs(GameManager.getUniversalTime() - currentTime) >= failureThreshold)
-        {
-            GameManager.LoseLife();
-            DisableClock();
-        }
+		if (Mathf.Abs(GameManager.getUniversalTime() - currentTime) >= failureThreshold)
+		{
+			GameManager.LoseLife();
+			DisableClock();
+		}
 	}
-
-    void UpdateTimeToSuccess()
-    {
+	
+	void UpdateTimeToSuccess()
+	{
 		timeToSuccess--;
 		HealthBar.fillAmount -= LifeMultiplier;
-
-        if (timeToSuccess <= 0)
-            DisableClock();
+		
+		if (timeToSuccess <= 0)
+			DisableClock();
 	}
-
+	
 	void OnMouseOver(){        
 		if (Input.GetMouseButton(0)) { //left button, speedup
-            timeChanger += clickChangeRate;
+			timeChanger += clickChangeRate;
 			if( timeChanger > 1){
 				currentTime += (int) timeChanger;
 				timeChanger = 0f;
 			}
-            DisplayTime();
+			DisplayTime();
 		}
 		else if (Input.GetMouseButton(1)) { //right button, slowdown
-            timeChanger += clickChangeRate;
+			timeChanger += clickChangeRate;
 			if( timeChanger > 1){
 				currentTime -= (int) timeChanger;
 				timeChanger = 0f;
 			}
-            DisplayTime();
+			DisplayTime();
 		}
 	}
-
+	
 	void OnMouseEnter(){
 		infoCard.gameObject.SetActive(true);
 	}
-
+	
 	void OnMouseExit(){
 		infoCard.gameObject.SetActive(false);
 	}
-
-    void DisableClock()
-    {
+	
+	void DisableClock()
+	{
 		GameManager.currentAmountClocks--;
-        CancelInvoke("UpdateTimeToSuccess");
-        CancelInvoke("UpdateClockTime");
-        gameObject.SetActive(false);
+		CancelInvoke("UpdateTimeToSuccess");
+		CancelInvoke("UpdateClockTime");
+		gameObject.SetActive(false);
 		infoCard.gameObject.SetActive(false);
-    }
-
-    public void StartClock()
+	}
+	
+	public void StartClock()
 	{
 		currentTime = GameManager.CurrentUniversalTime + Random.Range(minTimeOffset, maxTimeOffset);
-        timeToSuccess = Random.Range(minSuccessTime, maxSuccessTime);
-        failureThreshold = Random.Range(minThreshold, maxThreshold);
-
+		timeToSuccess = Random.Range(minSuccessTime, maxSuccessTime);
+		failureThreshold = Random.Range(minThreshold, maxThreshold);
+		
 		HealthBar.fillAmount = 1;
 		LifeMultiplier = (float) 1 / timeToSuccess;
-        TextThreshold.text = failureThreshold + " Min";
-
-        InvokeRepeating("UpdateClockTime", 0f, clockTickRate);
-        InvokeRepeating("UpdateTimeToSuccess", 0f, successTickRate);
-    }
+		TextThreshold.text = failureThreshold + " Min";
+		
+		InvokeRepeating("UpdateClockTime", 0f, clockTickRate);
+		InvokeRepeating("UpdateTimeToSuccess", 0f, successTickRate);
+	}
 }
--- Spring2015Jam/Assets/_Scripts/GameManager.cs
+++ Spring2015Jam/Assets/_Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour {
 	//Clock array
@@ -14,12 +15,15 @@ public class GameManager : MonoBehaviour {
 	public GameObject pauseMenu, gameOverMenu;
 	public static float universalTickRate;
 	public float clockSpawnRate = 30.0f;
-
+	public float difficultyIncreaseRate = 60.0f;
+	public int maxDifficulty = 8;
+	
 	// Use this for initialization
 	void Start () {
 		universalTickRate = 0.2f;
 		InvokeRepeating("UpdateUniversalClock", 0f, universalTickRate);
 		InvokeRepeating("AddClock", 0f, clockSpawnRate);
+		InvokeRepeating("IncreaseMaxClocks", 0f, difficultyIncreaseRate);
 		pauseMenu.SetActive(false);
 		gameOverMenu.SetActive(false);
 	}
@@ -33,76 +37,99 @@ public class GameManager : MonoBehaviour {
 			else
 				Pause ();
 		}
-
+		
 		if (Time.timeScale == 0)
 			return;
 		livesText.text = lives.ToString();
 		if (lives <= 0) {
 			GameOver();
 		}
-
-
-
+		
+		
+		
 	}
-
+	
 	void FixedUpdate () {
 		//Running this depends on Timescale
 	}
-
+	
 	void UpdateUniversalClock () {
 		CurrentUniversalTime += 1;
 		ParseTime (clockText, CurrentUniversalTime);
 	}
-
-
+	
+	
 	void ParseTime(Text t, int minutes){
 		gameHour = minutes / 60;
 		if (gameHour > 12) {
 			gameHour -=(12*(gameHour/12));
 		}
 		gameMinute = minutes % 60;
-
+		
 		t.text = string.Format ("{0:D2}:{1:D2}", gameHour, gameMinute);
 	}
-
-
+	
+	
 	void AddClock () {
-//		if ()
+		if (currentAmountClocks < maxClocks)
+		{
+			List<GameObject> inactives = new List<GameObject>();
+			foreach (GameObject clock in spawnableClocks)
+			{
+				if (!clock.activeInHierarchy)
+					inactives.Add(clock);
+			}
+			if (inactives.Count > 0)
+			{
+				int clockIndex = Random.Range (0, inactives.Count);
+				Debug.Log (clockIndex);
+				inactives[clockIndex].SetActive(true);	
+				inactives[clockIndex].GetComponent<Clock>().StartClock();
+				currentAmountClocks++;
+			}
+		}
 		//Instantiate(spawnableClocks[Random.Range (0, spawnableClocks.Length)]);  position?
-        //set the chosen one active
+		//set the chosen one active
+	}
+	
+	void IncreaseMaxClocks() {
+		if (maxClocks < maxDifficulty)
+			maxClocks *= 2;
+		else
+			CancelInvoke("IncreaseMaxClocks");
 	}
 	
 	public void AddLife () {
 		lives++;
 	}
-
+	
 	public static void LoseLife () {
 		lives--;
 	}
-
+	
 	public static int getUniversalTime (){
 		return CurrentUniversalTime;
 	}
-
+	
 	public void GameOver () {
 		Time.timeScale = 0;
 		gameOverMenu.SetActive(true);
 	}
-
+	
 	public void Pause () {
 		Time.timeScale = 0;
 		pauseMenu.SetActive(true);
 	}
-
+	
 	public void Unpause () {
 		pauseMenu.SetActive(false);
 		Time.timeScale = 1;
 	}
-
+	
 	public void TryAgain () {
 		Application.LoadLevel ("Gameplay");
 	}
-
+	
 	public void Quit () {
 		Application.Quit ();
 	}
